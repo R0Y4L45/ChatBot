98b3e9b7e8c9d0c6309a25d6c44a32e1cfb71a11
R0Y4L45 <106491312+R0Y4L45@users.noreply.github.com>
2022-11-05 19:58:30 +0400

    Initial commit message

---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using AIMLbot;
+
+namespace WpfApp5
+{
+    public partial class MainWindow : Window
+    {
+        Bot AI = new Bot();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            string s = Environment.CurrentDirectory;
+            s = s.Substring(0, s.Length - 25);
+            Directory.SetCurrentDirectory(s);
+
+            AI.loadSettings(); 
+            AI.loadAIMLFromFiles(); 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text += "You: " + textBox2.Text + '\n';
+            textBox.Text += '(' + DateTime.Now.ToShortTimeString() + ')' + "\n\n";
+            AI.isAcceptingUserInput = false; 
+            User myuser = new User("User", AI); 
+            AI.isAcceptingUserInput = true; 
+            Request r = new Request(textBox2.Text, myuser, AI); 
+            Result res = AI.Chat(r); 
+            textBox2.Clear();
+            textBox.Text += "Bonita: " + res.Output + '\n';
+            textBox.Text += '(' + DateTime.Now.ToShortTimeString() + ')' +"\n\n";
+            textBox.ScrollToEnd();
+        }
+
+        private void OnKeyDownHandler(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if(e.Key == System.Windows.Input.Key.Enter)
+            {
+                textBox.Text += "You: " + textBox2.Text + '\n';
+                textBox.Text += '(' + DateTime.Now.ToShortTimeString() + ')' + "\n\n";
+                AI.isAcceptingUserInput = false;
+                User myuser = new User("User", AI);
+                AI.isAcceptingUserInput = true;
+                Request r = new Request(textBox2.Text, myuser, AI);
+                Result res = AI.Chat(r);
+                textBox2.Clear();
+                textBox.Text += "Bonita: " + res.Output + '\n';
+                textBox.Text += '(' + DateTime.Now.ToShortTimeString() + ')' + "\n\n";
+                textBox.ScrollToEnd();
+            }
+        }
+    }
+}
